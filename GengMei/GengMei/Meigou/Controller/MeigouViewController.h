8a2304b36da1ae4fa5b1824dba860c50df024704
ColdChains <lax2012@qq.com>
2016-12-06 05:12:27 -0800

    创建根视图

---
+++ GengMei/GengMei/Meigou/Controller/MeigouViewController.h
@@ -0,0 +1,13 @@
+//
+//  MeigouViewController.h
+//  GengMei
+//
+//  Created by 冰凉的枷锁 on 2016/12/6.
+//  Copyright © 2016年 liuaoxiang. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface MeigouViewController : BaseViewController
+
+@end
---
+++ GengMei/GengMei/Mine/MineViewController.h
@@ -0,0 +1,13 @@
+//
+//  MineViewController.h
+//  GengMei
+//
+//  Created by 冰凉的枷锁 on 2016/12/6.
+//  Copyright © 2016年 liuaoxiang. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface MineViewController : BaseViewController
+
+@end
